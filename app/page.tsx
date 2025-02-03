6565c9bee863073dd31df2f2d23a75ec95f1f27f
Pouria Darandi <p.darandi@asredanesh.local>
2025-02-03 11:38:08 +0330

    added outline prop to btn component

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,48 @@ import { Button } from "@/components/ui/button";
 
 function RootPage() {
     return (
-        <div className="max-w-xs mx-auto">
+        <div className="w-fit mx-auto">
             <div className="w-full space-y-4">
-                <div className="flex items-center justify-center">
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
                     <Button className="mx-auto">Button</Button>
                     <Button className="mx-auto" isBusy>
                         Button
                     </Button>
+
+                    <Button className="mx-auto" outline>
+                        Button
+                    </Button>
+                    <Button className="mx-auto" outline isBusy>
+                        Button
+                    </Button>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
                     <Button className="mx-auto" btnType="dark">
                         Button
                     </Button>
                     <Button className="mx-auto" btnType="dark" isBusy>
                         Button
                     </Button>
+
+                    <Button className="mx-auto" btnType="dark" outline>
+                        Button
+                    </Button>
+                    <Button className="mx-auto" btnType="dark" outline isBusy>
+                        Button
+                    </Button>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
                     <Button className="mx-auto" btnType="light">
                         Button
                     </Button>
                     <Button className="mx-auto" btnType="light" isBusy>
+                        Button
+                    </Button>
+
+                    <Button className="mx-auto" btnType="light" outline>
+                        Button
+                    </Button>
+                    <Button className="mx-auto" btnType="light" outline isBusy>
                         Button
                     </Button>
                 </div>
