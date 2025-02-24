d584c7989ef5dd2e962ada979ba3a9b55b80acf6
Pouria Darandi <p.darandi@asredanesh.local>
2025-02-24 13:03:37 +0330

    reaname navigation to Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ToastProvider } from "@/context/ToastContext";
-import { Navigation } from "@/components/layout/Navigation";
+import { Navigation } from "@/components/layout/navigation";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
